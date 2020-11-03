ed968c989031f3576e78c1efb10c352c69d7dcd4
hansonjw <hansonjw@gmail.com>
2020-11-02 21:41:17 -0600

    Create landing page, home route for a single thesis

--- controllers/routes-home.js
+++ controllers/routes-home.js
@@ -42,4 +42,55 @@ router.get('/', (req, res) => {
 });
 
 
+router.get('/thesis/:id', (req, res) => {
+    Thesis.findOne({
+        where: { id: req.params.id},
+        attributes: [
+            'title',
+            'thesis_text',
+            'security_id'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'thesis_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Security,
+                attributes: ['name']
+            }
+        ]
+    })
+    .then(dbThesisData => {
+        if(!dbThesisData) {
+            res.status(404).jason({message: 'No Thesis found with this ID'});
+            return;
+        }
+
+        // serialize the data - what does this mean?
+        const thesis = dbThesisData.get({ plain: true });
+        console.log(thesis);
+
+        // pass data to handlebars template
+
+        res.render('aThesis', {
+            thesis
+            // loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+
 module.exports = router;
